83a01abe4d764cf5c9d96e09c61fd7d0b5c5e145
Alix Nouzillat <alix.nouzillat@ynov.com>
2017-04-01 15:43:06 +0200

    ajout tab bar + modif constraint/resto

--- app/src/main/java/com/ynov/android/gluciddiab/RestoActivity.java
+++ app/src/main/java/com/ynov/android/gluciddiab/RestoActivity.java
@@ -2,12 +2,38 @@ package com.ynov.android.gluciddiab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class RestoActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resto);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_settings:
+                Toast.makeText(RestoActivity.this, "Paramètre" , Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
